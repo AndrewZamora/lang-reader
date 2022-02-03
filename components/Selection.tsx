4b7c1a6c3aca2e3d906312b83db144abba78ffc9
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-02-03 23:57:38 +0900

    passed new word to flashcard state

--- components/Selection.tsx
+++ components/Selection.tsx
@@ -4,6 +4,7 @@ const Selection = ({word, onAddToDeck}) => {
   return <div>
     <h2>{word.segment}</h2>
     <p><span>Reading: </span>{word.hiragana}</p>
+    <button onClick={()=>onAddToDeck(word)}>Add to Anki Deck</button>
   </div>
 }
 
