7d1dce8acb2449caa8974d00052cd20e24b0f25d
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-02-04 22:36:02 +0900

    Added interface to Selection component

--- components/Selection.tsx
+++ components/Selection.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Selection = ({word, onAddToDeck}) => {
+interface SelectionProps {
+  word: {
+    segment: string,
+    hiragana: string,
+  },
+  onAddToDeck(word: object):void;
+}
+
+const Selection = (props: SelectionProps) => {
+  const { word, onAddToDeck } = props;
   return <div>
     <h2>{word.segment}</h2>
     <p><span>Reading: </span>{word.hiragana}</p>
-    <button onClick={()=>onAddToDeck(word)}>Add to Anki Deck</button>
+    <button onClick={() => onAddToDeck(word)}>Add to Anki Deck</button>
   </div>
 }
 
