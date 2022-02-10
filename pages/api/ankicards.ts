444781e46a3790d31bfcd038f8e83efcde1061b8
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-02-11 00:29:21 +0900

    got test deck sent via api

--- pages/api/ankicards.ts
+++ pages/api/ankicards.ts
@@ -1,33 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import AnkiExport from 'anki-apkg-export'
-import { saveAs } from 'file-saver'
 
 type Data = {
   name: string
 }
-  const exportAnkiDeck = async (flashcards) => {
-    let deckName = "testDeck"
-    const apkg = new AnkiExport(deckName)
-    console.log("apkg",apkg)
-    // flashCards.forEach((card) => {
-    //   const {
-    //     segment: front,
-    //     hiragana: back,
-    //   } = card
-    //   apkg.addCard(front, back)
-    // });
-    // const zip = await apkg
-    //   .save()
-    //   .catch((err) => console.log(err.stack || err))
-    // saveAs(zip, `${deckName}.apkg`)
-    // console.log("done")
-  }
+
+const exportAnkiDeck = async (flashcards) => {
+  let deckName = "testDeck"
+  const apkg = new AnkiExport(deckName)
+  console.log("apkg", apkg)
+  flashcards.forEach((card) => {
+    const {
+      segment: front,
+      hiragana: back,
+    } = card
+    apkg.addCard(front, back)
+  });
+  const zip = await apkg
+    .save()
+    .catch((err) => console.log(err.stack || err))
+  return zip
+  // saveAs(zip, `${deckName}.apkg`)
+  console.log("done")
+}
 
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'here is your deck' })
+  const file = await exportAnkiDeck([{ segment: 'test', hiragana: 'てすと' }])
+  res.writeHead(200, {
+    "Content-Type": "application/octet-stream",
+    "Content-Disposition": "attachment; filename=" + 'test.apkg'
+  })
+  res.send(file)
 }
