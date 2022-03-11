ee2d4122f8b5ed5d7ccb46530ffa7bbcbaf3ecd6
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-03-11 21:22:48 +0900

    Fixed styles issue on listReaders

--- components/ListReaders.tsx
+++ components/ListReaders.tsx
@@ -6,24 +6,21 @@ interface ListReadersProps {
   readers: { name: string, lang: string, id: string }[]
 }
 
-const useStyles = makeStyles({
-  readerItem: {
-    padding: '1em 1.5em',
-  },
-  readerContainer: {
-    marginBottom: '0.5em',
-    cursor: 'pointer'
-  }
-})
-
 export default function ListReaders(props: ListReadersProps) {
+  const useStyles = makeStyles({
+    readerItem: {
+      padding: '1em 1.5em',
+    },
+    readerContainer: {
+      marginBottom: '0.5em',
+      cursor: 'pointer'
+    }
+  })
   const classes = useStyles()
   const { readers } = props
   return readers.length ? (
-    
-      readers.map(reader => {
-        return <Paper className={classes.readerContainer} variant="outlined"><div className={classes.readerItem}>Name: {reader.name} Lang: {reader.lang}</div></Paper>
-      })
-    
+    readers.map(reader => {
+      return <Paper className={classes.readerContainer} variant="outlined"><div className={classes.readerItem}>Name: {reader.name} Lang: {reader.lang}</div></Paper>
+    })
   ) : (<div>No readers</div>)
 }
