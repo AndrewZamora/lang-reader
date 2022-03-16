dad13b5f357708ca3cd387b76026630cab38b17d
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-03-16 22:57:18 +0900

    displayed segments on reader page

--- pages/Reader.tsx
+++ pages/Reader.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useCallback } from 'react'
 import type { NextPage } from 'next'
 import Container from '@mui/material/Container'
 import { useRouter } from 'next/router'
-
+import { makeStyles } from '@mui/styles'
 
 const Reader: NextPage = () => {
   const router = useRouter()
@@ -13,14 +13,30 @@ const Reader: NextPage = () => {
   useEffect(() => {
     console.log("useEffect reader page")
     const localData = localStorage.getItem(`langReader-${id}`)
-    const localReader  = JSON.parse(localData)
-    console.log(localReader)
+    const localReader = JSON.parse(localData)
+    console.log(localReader, id)
     setReader(localReader)
-  },[])
+  }, [])
 
+  const useStyles = makeStyles({
+    segment: {
+      border: 'lightblue solid 1px',
+      cursor: 'pointer',
+      fontSize: '20px',
+      '&:hover': {
+        background: 'lightblue'
+      }
+    }
+  })
+
+  const classes = useStyles()
+  const handleClick=(segment: string) => {
+    console.log(segment)
+  }
   return (
     <Container maxWidth="lg">
       <div>Reader Page</div>
+      {reader && reader.segments.map((segment, index) => <span className={segment.isWordLike ? classes.segment : undefined } onClick={(()=> handleClick(segment.segment))} key={segment.segment + index}>{segment.segment}</span>)}
     </Container>
   )
 }
