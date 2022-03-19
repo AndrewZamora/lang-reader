732b82591721558e5b755b242441d1b5c986df5c
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-03-19 23:59:47 +0900

    Added source input to create reader

--- components/LangInput.tsx
+++ components/LangInput.tsx
@@ -11,6 +11,7 @@ const LangInput = (props: LangInputProps) => {
   const { handleOutput } = props
   const [input, setInput] = useState('')
   const [name, setName] = useState('')
+  const [source, setSource] = useState('')
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value)
@@ -18,6 +19,10 @@ const LangInput = (props: LangInputProps) => {
 
   const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
+  }
+
+    const handleSource = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSource(event.target.value)
   }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -35,7 +40,10 @@ const LangInput = (props: LangInputProps) => {
   const useStyles = makeStyles({
     btn: {
       marginTop: '20px'
-    }
+    },
+    input: {
+      marginBottom: '10px'
+    },
   })
 
   const classes = useStyles()
@@ -45,13 +53,24 @@ const LangInput = (props: LangInputProps) => {
       <form noValidate autoComplete='off' onSubmit={event => handleSubmit(event)}>
         <FormControl fullWidth sx={{ m: 1 }}>
           <TextField
+            className={classes.input}
             id="reader-name-input"
             label="Reader Name"
             variant="outlined"
             onChange={event => handleName(event)}
+            value={name}
           />
           <TextField
-            id="filled-multiline-static"
+            className={classes.input}
+            id="reader-source-input"
+            label="Reader Source"
+            variant="outlined"
+            onChange={event => handleSource(event)}
+            value={name}
+          />
+          <TextField
+            className={classes.input}
+            id="reader-text-input"
             label="Reader Text"
             multiline
             variant="outlined"
