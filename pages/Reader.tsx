a41f67867f69bb4f0688ae531455fe8a6b9e6c2b
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-03-21 23:32:26 +0900

    display word selection on reader page

--- pages/Reader.tsx
+++ pages/Reader.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect, useCallback } from 'react'
 import type { NextPage } from 'next'
-import Container from '@mui/material/Container'
+import { Paper, Container, Grid } from '@mui/material'
 import { useRouter } from 'next/router'
 import { makeStyles } from '@mui/styles'
 
@@ -9,6 +9,7 @@ const Reader: NextPage = () => {
   const router = useRouter()
   const { id } = router.query
   const [reader, setReader] = useState<object | null>(null)
+  const [selection, setSelection] = useState('')
 
   useEffect(() => {
     console.log("useEffect reader page")
@@ -27,17 +28,31 @@ const Reader: NextPage = () => {
       '&:hover': {
         background: 'lightblue'
       }
+    },
+    h2: {
+      border: "pink solid 2px"
     }
   })
 
   const classes = useStyles()
-  const handleClick=(segment: string) => {
+  const handleClick = (segment: string) => {
     console.log(segment)
+    setSelection(segment)
   }
   return (
     <Container maxWidth="lg">
       <div>Reader Page</div>
-      {reader && reader.segments.map((segment, index) => <span className={segment.isWordLike ? classes.segment : undefined } onClick={(()=> handleClick(segment.segment))} key={segment.segment + index}>{segment.segment}</span>)}
+      <Grid container spacing={1} >
+        <Grid item xs={5}>
+          <Paper>
+            {reader && reader.segments.map((segment, index) => <span className={segment.isWordLike ? classes.segment : undefined} onClick={(() => handleClick(segment.segment))} key={segment.segment + index}>{segment.segment}</span>)}
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <h2>{selection}</h2>
+        </Grid>
+
+      </Grid>
     </Container>
   )
 }
