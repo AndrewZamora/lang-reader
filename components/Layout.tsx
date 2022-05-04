b6f67b4b41d51b9ec2845aae640c03ba7a0d3513
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-05-04 23:41:57 +0900

    Added nav to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,109 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
-// import styles from './layout.module.css'
+import {AppBar, Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem,ListItemIcon,ListItemText,Toolbar,Typography} from '@mui/material'
+import {GitHub, Menu} from '@mui/icons-material'
+import { jsx } from '@emotion/react'
 
-export default function Layout({ children }) {
+
+const drawerWidth = 240
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window
+}
+export default function Layout(props) {
+  const { window, children } = props
+  const [mobileOpen, setMobileOpen] = useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {[{text:'Github', icon: <GitHub/>}].map((icon, index) => (
+          <ListItem button key={icon.text}>
+            <ListItemIcon>
+              {icon.icon}
+            </ListItemIcon>
+            <ListItemText primary={icon.text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
+
+  const container = window !== undefined ? () => window().document.body : undefined
+
   return (
-    <>
-      {/* <main className={styles.main}>{children}</main> */}
-      
-      <main>{children}</main>
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Lang Reader
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
   )
 }
