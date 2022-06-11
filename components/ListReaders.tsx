ae696c6fca6a407c1f95726d7dcef6e279ba3da5
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-06-11 23:28:13 +0900

    removed more makeStyles

--- components/ListReaders.tsx
+++ components/ListReaders.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Paper } from '@mui/material'
-import { makeStyles } from '@mui/styles'
 import { DeleteForever } from '@mui/icons-material'
 import IconButton from '@mui/material/IconButton'
+import styles from './ListReaders.module.css'
 
 interface ListReadersProps {
   readers: { name: string, lang: string, id: string }[],
@@ -12,30 +12,17 @@ interface ListReadersProps {
 
 export default function ListReaders(props: ListReadersProps) {
   const { readers, handleClick, handleDelete } = props
-  const useStyles = makeStyles({
-    readerItem: {
-      padding: '1em 1.5em',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between'
-    },
-    readerContainer: {
-      marginBottom: '0.5em',
-      cursor: 'pointer',
-    }
-  })
   const deleteReader = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: string) => {
     event.stopPropagation()
     handleDelete(id)
   }
-  const classes = useStyles()
   const list = () => {
     return readers.map(reader => {
       return (<Paper key={reader.id}
         onClick={() => handleClick(reader.id)}
-        className={classes.readerContainer}
+        className={styles.readerContainer}
         variant="outlined">
-        <div className={classes.readerItem}>
+        <div className={styles.readerItem}>
           <div>Name: {reader.name} Lang: {reader.lang}</div>
           <div>
             <IconButton
--- components/Selection.tsx
+++ components/Selection.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Button, IconButton, FormControl, TextField, } from '@mui/material'
 import { Edit as EditIcon } from '@mui/icons-material'
-import { makeStyles } from '@mui/styles'
+import styles from './Selection.module.css'
 
 interface SelectionProps {
   word: {
@@ -29,38 +29,7 @@ const Selection = (props: SelectionProps) => {
     setUpdate(word)
   }, [word])
 
-  const useStyles = makeStyles({
-    selectionContainer: {
-      width: '100%',
 
-    },
-    settingsIconContainer: {
-      display: 'flex',
-      justifyContent: 'flex-end',
-      padding: '10px 10px 0 0'
-    },
-    selectionContent: {
-      margin: '0 10%'
-    },
-    segment: {
-      textAlign: 'center'
-    },
-    button: {
-      margin: "0 auto",
-      display: "block"
-    },
-    inputs: {
-      paddingBottom: "10px"
-    },
-    inputsContainer: {
-      padding: "20px"
-    },
-    formBtn: {
-      marginLeft: "auto"
-    }
-  })
-
-  const classes = useStyles()
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -74,46 +43,46 @@ const Selection = (props: SelectionProps) => {
   }
 
   return (
-    <div className={classes.selectionContainer}>
-      <div className={classes.settingsIconContainer}>
+    <div className={styles.selectionContainer}>
+      <div className={styles.settingsIconContainer}>
         <IconButton aria-label="settings" size="small" onClick={() => setShowEditInputs(!showEditInputs)}>
           <EditIcon fontSize="inherit" />
         </IconButton>
       </div>
-      {!showEditInputs && <div className={classes.selectionContent}>
-        <h2 className={classes.segment}>{word.segment}</h2>
+      {!showEditInputs && <div className={styles.selectionContent}>
+        <h2 className={styles.segment}>{word.segment}</h2>
         <p><span>Reading: </span>{word.hiragana}</p>
         {word.definition && <p><span>Definition: </span>{word.definition}</p>}
-        {deck.includes(word.segment) ? <Button variant="outlined" className={classes.button} onClick={() => onRemove(word)}>Remove from flashcards</Button> : <Button variant="outlined" className={classes.button} onClick={() => onAdd(word)}>Add to flashcards</Button>}
+        {deck.includes(word.segment) ? <Button variant="outlined" className={styles.button} onClick={() => onRemove(word)}>Remove from flashcards</Button> : <Button variant="outlined" className={styles.button} onClick={() => onAdd(word)}>Add to flashcards</Button>}
       </div>}
       {showEditInputs && <form onSubmit={event => handleSubmit(event)}>
 
-        <FormControl fullWidth sx={{ m: 1 }} className={classes.inputsContainer}>
+        <FormControl fullWidth sx={{ m: 1 }} className={styles.inputsContainer}>
           <TextField
             label="Segment"
             variant="outlined"
             value={update.segment}
-            className={classes.inputs}
+            className={styles.inputs}
             onChange={event => setUpdate({...update, segment: event.target.value})}
           />
           <TextField
             label="Reading"
             variant="outlined"
             value={update.hiragana}
-            className={classes.inputs}
+            className={styles.inputs}
             onChange={event => setUpdate({...update, hiragana: event.target.value})}
           />
           <TextField
             label="Definition"
             variant="outlined"
             value={update.definition ? update.definition : ''}
-            className={classes.inputs}
+            className={styles.inputs}
             onChange={event => setUpdate({...update, definition: event.target.value})}
           />
-          <Button className={classes.formBtn} variant="outlined" type="submit">Update</Button>
-          <Button className={classes.formBtn} onClick={()=>handleDelete(update)} variant="outlined">Delete</Button>
-          <Button className={classes.formBtn} onClick={()=>onMerge(update,'right')} variant="outlined">Merge Right</Button>
-          <Button className={classes.formBtn} onClick={()=>onMerge(update,'left')} variant="outlined">Merge left</Button>
+          <Button className={styles.formBtn} variant="outlined" type="submit">Update</Button>
+          <Button className={styles.formBtn} onClick={()=>handleDelete(update)} variant="outlined">Delete</Button>
+          <Button className={styles.formBtn} onClick={()=>onMerge(update,'right')} variant="outlined">Merge Right</Button>
+          <Button className={styles.formBtn} onClick={()=>onMerge(update,'left')} variant="outlined">Merge left</Button>
         </FormControl>
       </form>}
     </div>
