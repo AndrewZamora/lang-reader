6cb682bcdba563e2c17f566a0c87d950a0302359
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-07-10 00:43:42 +0900

    got flashcards from all decks

--- pages/Flashcards.tsx
+++ pages/Flashcards.tsx
@@ -3,13 +3,17 @@ import type { NextPage } from 'next'
 
 const Flashcards: NextPage = () => {
   const [cards, setCards] = useState([])
-  
+
   useEffect(() => {
-    console.log("useEffect 1 ")
     const storedReaders = localStorage.getItem('langReaders')
     if (storedReaders) {
       const ids = JSON.parse(storedReaders).map(reader => reader.id)
-      console.log(ids)
+      ids.forEach(id => {
+        const storedReader = localStorage.getItem(`langReader-${id}`)
+        const flashcards = JSON.parse(storedReader).deck
+        console.log(flashcards)
+        setCards([...cards, flashcards])
+      })
     }
   }, [])
   return (
