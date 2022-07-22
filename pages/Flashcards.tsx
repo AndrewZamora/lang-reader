27cd5bca1793170ff3ab54764f1eef46dfe90a36
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-07-23 00:30:00 +0900

    feature: displayed global set of flashcards

--- pages/Flashcards.tsx
+++ pages/Flashcards.tsx
@@ -1,24 +1,32 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container } from '@mui/material'
+import WordTable from '../components/WordTable'
 import type { NextPage } from 'next'
 
 const Flashcards: NextPage = () => {
   const [cards, setCards] = useState([])
 
+  const getAllFlashcards = (ids) => {
+    let flashcards = []
+    ids.forEach(id => {
+      const storedReader = localStorage.getItem(`langReader-${id}`)
+      flashcards = [...flashcards, ...JSON.parse(storedReader).deck]
+    })
+    setCards(flashcards)
+  }
+
   useEffect(() => {
     const storedReaders = localStorage.getItem('langReaders')
     if (storedReaders) {
       const ids = JSON.parse(storedReaders).map(reader => reader.id)
-      ids.forEach(id => {
-        const storedReader = localStorage.getItem(`langReader-${id}`)
-        const flashcards = JSON.parse(storedReader).deck
-        console.log(flashcards)
-        setCards([...cards, flashcards])
-      })
+      getAllFlashcards(ids)
     }
   }, [])
+
   return (
-    <div>
-    </div>
+    <Container maxWidth="lg">
+      {cards.length ? <WordTable deck={cards} /> : <h2>No flashcards have been added.</h2>}
+    </Container>
   )
 }
 export default Flashcards
