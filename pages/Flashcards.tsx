09c1431518136f829bcb52acd34688af4f7e9c25
AndrewZamora <andrewmichaelzamora@gmail.com>
2023-01-06 22:02:20 +0900

    chore: fixed type errors

--- pages/Flashcards.tsx
+++ pages/Flashcards.tsx
@@ -5,13 +5,13 @@ import type { NextPage } from 'next'
 import Layout from '../components/Layout'
 
 const Flashcards: NextPage = () => {
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState<[] | object[] >([])
 
-  const getAllFlashcards = (ids) => {
-    let flashcards = []
-    ids.forEach(id => {
+  const getAllFlashcards = (ids: [string]) => {
+    let flashcards: Array<object> = []
+    ids.forEach((id: string) => {
       const storedReader = localStorage.getItem(`langReader-${id}`)
-      const parsedReader = JSON.parse(storedReader)
+      const parsedReader = storedReader ? JSON.parse(storedReader) : null
       if (parsedReader.deck && parsedReader.deck.length) {
         flashcards = [...flashcards, ...parsedReader.deck]
       }
@@ -24,7 +24,7 @@ const Flashcards: NextPage = () => {
   useEffect(() => {
     const storedReaders = localStorage.getItem('langReaders')
     if (storedReaders) {
-      const ids = JSON.parse(storedReaders).map(reader => reader.id)
+      const ids = JSON.parse(storedReaders).map((reader: object) => reader.id)
       getAllFlashcards(ids)
     }
   }, [])
