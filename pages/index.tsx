6d0ce956df4e8bd6e4464e3b59c04ce8b480ff45
AndrewZamora <andrewmichaelzamora@gmail.com>
2023-01-28 11:02:23 +0900

    refractor: removed extra useEffect and unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,25 +14,25 @@ interface Reader {
   id: string
 }
 
+const getReaders = () => {
+  if (typeof window !== 'undefined') {
+    const storedReaders = localStorage.getItem('langReaders')
+    if (storedReaders) {
+      return JSON.parse(storedReaders)
+    }
+  }
+  return []
+}
+
 const Home: NextPage = () => {
   const [readers, setReaders] = useState<Reader[]>([])
 
-  useEffect(() => {
-    console.log("useEffect 1 ")
-    const storedReaders = localStorage.getItem('langReaders')
-    if (storedReaders) {
-      setReaders(JSON.parse(storedReaders))
-    }
-  }, [])
-
   const handleClick = (id: string) => {
-    console.log(id)
     router.push(`/Reader?id=${id}`)
   }
 
   const deleteReader = (id: string) => {
-    const storedReaders = localStorage.getItem('langReaders')
-    const readers = JSON.parse(storedReaders)
+    const readers = getReaders()
     const updatedReaders = readers.filter(reader => reader.id !== id)
     setReaders(updatedReaders)
     localStorage.setItem('langReaders', JSON.stringify(updatedReaders))
@@ -44,14 +44,21 @@ const Home: NextPage = () => {
     { icon: <CreateSharp />, name: 'Create Reader', onClick: () => { router.push('/CreateReader') } },
   ]
 
+  const createReaderList = () => {
+    const readersList = readers.length ? readers : getReaders()
+    return (
+      <ListReaders
+        readers={readersList}
+        handleClick={(id: string) => handleClick(id)}
+        handleDelete={(id: string) => deleteReader(id)} />
+    )
+  }
+
   return (
     <Layout>
       <Container maxWidth="lg">
         <h2> All Readers</h2>
-        <ListReaders
-          readers={readers}
-          handleClick={(id: string) => handleClick(id)}
-          handleDelete={(id: string) => deleteReader(id)} />
+        {createReaderList()}
         {/* <SpeedDial
           ariaLabel="create reader"
           sx={{ position: 'absolute', bottom: 16, right: 16 }}
