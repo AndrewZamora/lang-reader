31bdcdd8f9abe07c11aa0d0cc400eb74a69db46f
AndrewZamora <andrewmichaelzamora@gmail.com>
2023-02-28 22:38:36 +0900

    bug: typescript issue

--- pages/api/definition.ts
+++ pages/api/definition.ts
@@ -8,7 +8,10 @@ export default async function handler(
   const response = fetch(`https://jisho.org/api/v1/search/words?keyword=${word}`).catch(error => console.log(error))
   const result = await response
   try {
-    const json = await result.json()
+    let json = {"error": "No JSON"};
+    if(result?.json){
+      json =  await result.json()
+    } 
     res.json(json)
   } catch (error) {
     console.log(result)
